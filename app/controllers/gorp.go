abe1f5327eadd3cf05f957e5640b040cbc7a5f7c
funnythingz <funnythingz69+github@gmail.com>
2014-09-12 15:12:35 +0900

    added gorp

---
+++ app/controllers/gorp.go
@@ -0,0 +1,80 @@
+package controllers
+
+import (
+        "database/sql"
+        "github.com/coopernurse/gorp"
+        _ "github.com/mattn/go-sqlite3"
+        "github.com/revel/revel"
+        "pin/app/models"
+       )
+
+var (
+        DbMap *gorp.DbMap
+    )
+
+func InitDB() {
+
+    db, err := sql.Open("sqlite3", "./local/app.db")
+
+    if err != nil {
+        panic(err.Error())
+    }
+
+    DbMap = &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+
+    // TODO: ここでテーブルを定義する
+    t := DbMap.AddTable(models.Pin{}).SetKeys(true, "Id")
+    t.ColMap("Title").MaxSize = 20
+
+    DbMap.DropTables()
+    DbMap.CreateTables()
+
+}
+
+type GorpController struct {
+    *revel.Controller
+    Transaction *gorp.Transaction
+}
+
+func (c *GorpController) Begin() revel.Result {
+    txn, err := DbMap.Begin()
+
+    if err != nil {
+        panic(err)
+    }
+
+    c.Transaction = txn
+    return nil
+}
+
+func (c *GorpController) Commit() revel.Result {
+    if c.Transaction == nil {
+        return nil
+    }
+
+    err := c.Transaction.Commit()
+
+    if err != nil && err != sql.ErrTxDone {
+        panic(err)
+    }
+
+    c.Transaction = nil
+    return nil
+
+}
+
+func (c *GorpController) Rollback() revel.Result {
+    if c.Transaction == nil {
+        return nil
+    }
+
+    err := c.Transaction.Rollback()
+
+    if err != nil && err != sql.ErrTxDone {
+        panic(err)
+    }
+
+    c.Transaction = nil
+    return nil
+
+}
