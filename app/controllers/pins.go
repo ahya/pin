c798bf73bb4ba2bbc26560af50985cd0cc1d4134
funnythingz <funnythingz69+github@gmail.com>
2014-09-12 15:12:14 +0900

    tweaks pins.go

--- app/controllers/pins.go
+++ app/controllers/pins.go
@@ -1,13 +1,12 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+        "github.com/revel/revel"
+        "pin/app/models"
+       )
 
 type Pins struct {
 	*revel.Controller
-}
-
-type Pin struct {
-    Title, Memo string
 }
 
 func (c Pins) Index() revel.Result {
@@ -16,10 +15,10 @@ func (c Pins) Index() revel.Result {
     revel.TRACE.Printf("%s", c.Params.Get("inputMemo"))
 
     // TODO: sample
-    pinList := []Pin{
-        Pin{Title: "たいとる1", Memo: "めもめも1"},
-        Pin{Title: "たいとる2", Memo: "めもめも2"},
-        Pin{Title: "たいとる3", Memo: "めもめも3"},
+    pinList := []models.Pin{
+        models.Pin{Title: "たいとる1", Memo: "めもめも1"},
+        models.Pin{Title: "たいとる2", Memo: "めもめも2"},
+        models.Pin{Title: "たいとる3", Memo: "めもめも3"},
     }
 
 	return c.Render(pinList)
