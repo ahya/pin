7a8d98e066d9dbed161a72053bde418f926e35fa
funnythingz <funnythingz69+github@gmail.com>
2014-11-02 17:15:58 +0900

    added models/Favorite

---
+++ app/models/favorite-view.go
@@ -0,0 +1,5 @@
+package models
+
+type FavoriteView struct {
+    Favorites []Favorite
+}
---
+++ app/models/favorite.go
@@ -0,0 +1,13 @@
+package models
+
+type Favorite struct {
+	Id, Created, PinId  int64
+}
+
+func (f Favorite) All() FavoriteView {
+
+    var favorites []Favorite
+    DB.Find(&favorites)
+
+    return FavoriteView{Favorites: favorites}
+}
